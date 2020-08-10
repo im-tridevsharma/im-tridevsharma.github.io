eb4c3f1dfdb31ce609a00f186f95d52ba5990b73
im-tridevsharma <62466333+im-tridevsharma@users.noreply.github.com>
2020-08-10 10:01:38 +0530

    Update del-script.js

--- scripts/del-script.js
+++ scripts/del-script.js
@@ -7,11 +7,16 @@ function deleteIt(noteId) {
             notes = JSON.parse(notes);
             notes.forEach((item, index) => {
                 if(item.noteId === Number(noteId)) {
-                    notes.splice(index);
+                    notes.splice(index,1);
                     event.target.parentNode.parentNode.remove();
                 }
             });
-            if(notes.length == 0){location.reload()}
+            if(notes.length == 0){
+                let error = document.createElement('p');
+                error.classList.add("no-notes");
+                error.innerText = "No notes created yet!";
+                document.getElementById('notes-area').appendChild(error);
+            }
             localStorage.setItem('notes', JSON.stringify(notes));
         }
     }
