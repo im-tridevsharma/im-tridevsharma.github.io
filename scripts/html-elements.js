bbb1fa2a9d3f06b145abc3c2a2fa34a0fca6d40a
im-tridevsharma <62466333+im-tridevsharma@users.noreply.github.com>
2020-08-10 09:42:46 +0530

    Update html-elements.js

--- scripts/html-elements.js
+++ scripts/html-elements.js
@@ -135,23 +135,6 @@ let completeHtml = () => {
     return container;
 }
 
-function deleteIt(noteId) {
-    if(noteId !== '') {
-        let notes = localStorage.getItem("notes");
-        if(notes !== null && notes !== '[]') {
-            notes = JSON.parse(notes);
-            notes.forEach((item, index) => {
-                if(item.noteId === Number(noteId)) {
-                    notes.splice(index);
-                    event.target.parentNode.parentNode.remove();
-                }
-            });
-            if(notes.length == 0){location.reload()}
-            localStorage.setItem('notes', JSON.stringify(notes));
-        }
-    }
-}
-
 const html = completeHtml;
 
 export {html, deleteIt};
