db899f807dc106d75e9d1be1003ee4e9ee4a04ec
im-tridevsharma <62466333+im-tridevsharma@users.noreply.github.com>
2020-08-10 13:54:05 +0530

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,72 @@
+
+//chat control
+localStorage.setItem("user","dummy");
+let allowed_users = ["juhi","tridev"];
+document.getElementsByClassName('form-con')[0].style.display = "none";
+function login() {
+  let nuser = prompt("Who are you?");
+  if(allowed_users.indexOf(nuser.toLowerCase()) !== -1){
+    localStorage.setItem("user",nuser);
+    init();
+  }
+}
+
+function init() {
+  let user = localStorage.getItem("user");
+  if(allowed_users.indexOf(user.toLowerCase()) !== -1){
+
+    document.getElementsByClassName('form-con')[0].style.display = "block";
+   
+   let chatform = document.getElementById('chatform');
+    chatform.onsubmit = (event) => {
+        event.preventDefault();
+
+        let user = localStorage.getItem("user");
+        if(user === null && allowed_users.indexOf(user)<0) {
+            alert("You are not allowed to chat here");
+        }else {
+            let messageInput = document.getElementById('msg');
+            message = messageInput.value.trim();
+            if(message !== ''){
+                firebase.database().ref("messages").push().set({
+                    "sender" : user,
+                    "message": message
+                });
+            }
+            messageInput.value = "";
+        }
+
+        return false;
+    }
+
+    firebase.database().ref("messages").on("child_added", (data) => {
+        let user = localStorage.getItem("user");
+        let color = "";
+        if(user!==null && user===data.val().sender){
+          color = "self";
+        }
+        let list = document.getElementById('show');
+        let html = "<p class="+color+"><b>"+data.val().sender+"</b> :";
+        html += "<i id='id"+data.key+"'>"+data.val().message+"</i>";
+        if(user!==null && user===data.val().sender){
+          html += "<i class='fas fa-trash' data-id='"+data.key+"' onclick='removeMe(this)'></i></p>";
+        }
+        list.innerHTML += html;
+    });
+
+    function removeMe(self) {
+      let id = self.getAttribute("data-id");
+      firebase.database().ref("messages").child(id).remove();
+    }
+
+    firebase.database().ref("messages").on("child_removed", (data) => {
+      let removed = document.getElementById("id"+data.key);
+      removed.innerHTML = "This message has been removed.";
+      removed.parentNode.classList.add("removed");
+    });
+
+  }
+}
+
+init();
+
